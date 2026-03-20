eeb1f38b24663e6a5ae52d3b535e6db07703d85f
Mobin Amiri <amirialimobin@gmail.com>
2026-03-20 14:31:50 +0100

    added the header to the page

--- task-manager/src/header/Header.jsx
+++ task-manager/src/header/Header.jsx
@@ -1,5 +1,9 @@
+import './Header.css'
+
 export function Header() {
   return (
-    
+    <div className='Header'>
+      <h1>TaskFlow</h1>
+    </div>
   );
 }
--- task-manager/src/pages/HomePage.jsx
+++ task-manager/src/pages/HomePage.jsx
@@ -1,14 +1,20 @@
 import './HomePage.css';
+import { Header } from '../header/Header';
 
 export function HomePage() {
   return (
-    <div className='homepage-container'>
-      <div className="sidebar">
-        <h1>SideBar</h1>
+    <>
+      <div className='homepage-container'>
+        <Header />
+        <div className='main-container'>
+          <div className="sidebar">
+            <h1>SideBar</h1>
+          </div>
+          <div className="homepage">
+            <h2>HomePage</h2>
+          </div>
+        </div>
       </div>
-      <div className="homepage">
-        <h2>HomePage</h2>
-      </div>
-    </div>
+    </>
   )
 }
