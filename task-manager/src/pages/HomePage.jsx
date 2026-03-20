8c58ba7155c38e2f2b6a00d27bdeb05b5598ac07
Mobin Amiri <amirialimobin@gmail.com>
2026-03-20 13:38:01 +0100

    made the homepage lay out

---
+++ task-manager/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import './HomePage.css';
+
+export function HomePage() {
+  return (
+    <div className='homepage-container'>
+      <div className="sidebar">
+        <h1>SideBar</h1>
+      </div>
+      <div className="homepage">
+        <h2>HomePage</h2>
+      </div>
+    </div>
+  )
+}
