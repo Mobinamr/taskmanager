761a1319dfaf0185261a38ef90163b595762df52
Mobin Amiri <amirialimobin@gmail.com>
2026-03-23 14:15:47 +0100

    fixed the header

--- task-manager/src/header/Header.jsx
+++ task-manager/src/header/Header.jsx
@@ -4,10 +4,11 @@ export function Header() {
   return (
     <div className='Header'>
       <div className='Left-part'>
-        <h1>Task Manager</h1>
+        <img src="" alt="" />
+        <h1>TaskFlow</h1>
       </div>
       <div className='Right-part'>
-        <input className='input' placeholder='search tasks' type="text" />
+        <input className='input' placeholder='search tasks...' type="text" />
       </div>
     </div>
   );
