60604f88e0376166ff3092184c29fd6b879b6dc3
Mobin Amiri <amirialimobin@gmail.com>
2026-03-23 15:09:46 +0100

    added html to the sidebar

--- task-manager/src/pages/HomePage.jsx
+++ task-manager/src/pages/HomePage.jsx
@@ -7,8 +7,15 @@ export function HomePage() {
       <div className='homepage-container'>
         <Header />
         <div className='main-container'>
-          <div className="sidebar">
-            <h1>SideBar</h1>
+          <div className='sidebar'>
+            <h1 className='sidebar-title' >CATEGORIES</h1>
+            <div className='buttons-container'>
+              <button>All tasks</button>
+              <button>Work</button>
+              <button>Personal</button>
+              <button className='Learning-button'>Learning</button>
+            </div>
+            <button className='category-button'>New category</button>
           </div>
           <div className="homepage">
             <h2>HomePage</h2>
