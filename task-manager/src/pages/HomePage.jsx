ebe4b8bd0fbf9fa84d50b8f2da119204fc22f84e
Mobin Amiri <amirialimobin@gmail.com>
2026-03-25 13:08:19 +0100

    added the homepage buttons

--- task-manager/src/pages/HomePage.jsx
+++ task-manager/src/pages/HomePage.jsx
@@ -14,7 +14,23 @@ export function HomePage() {
         <div className='main-container'>
           <SideBar setTitle={setTitle} />
           <div className="homepage">
-            <h2 className='homepage-title'>{title}</h2>
+            <div className='homepage-header'>
+              <div>
+                <h2 className='homepage-title'>{title}</h2>
+                <p className='homepage-subtitle'>2 tasks &ndash; 2 due today</p>
+              </div>
+              <div className='homepage-button-container'>
+                <select className='date-button' name="date" id="">
+                  <option value="date">Sort by due date</option>
+                  <option value="priority">Sort by priority</option>
+                  <option value="alphabetical">Sort by name</option>
+                </select>
+
+                <button className='add-task-button'>
+                  Add Task
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- task-manager/src/pages/SideBar.jsx
+++ task-manager/src/pages/SideBar.jsx
@@ -6,15 +6,34 @@ import { FaRegHeart } from "react-icons/fa";
 import { FiBookOpen } from "react-icons/fi";
 import { GoPlus } from "react-icons/go";
 
-export function SideBar( {setTitle} ) {
+export function SideBar({ setTitle }) {
   return (
     <div className='sidebar'>
       <h1 className='sidebar-title' >CATEGORIES</h1>
       <div className='buttons-container'>
-        <button onClick={() => {setTitle('All tasks')}}> <LayoutGrid className='button-icon' /> All tasks</button>
-        <button onClick={() => {setTitle('Work')}}> <TbBriefcase2 className='work-button-icon' /> Work</button>
-        <button onClick={() => {setTitle('Personal')}}> <FaRegHeart className='button-icon' /> Personal</button>
-        <button className='Learning-button' onClick={() => {setTitle('Learning')}}> <FiBookOpen className='button-icon' /> Learning</button>
+        <button onClick={() => {
+          setTitle('All tasks')
+        }}> <LayoutGrid className='button-icon' />
+          All tasks
+        </button>
+
+        <button onClick={() => {
+          setTitle('Work')
+        }}> <TbBriefcase2 className='work-button-icon' />
+          Work
+        </button>
+
+        <button onClick={() => {
+          setTitle('Personal')
+        }}> <FaRegHeart className='button-icon' />
+          Personal
+        </button>
+
+        <button className='Learning-button' onClick={() => {
+          setTitle('Learning')
+        }}> <FiBookOpen className='button-icon' />
+          Learning
+        </button>
       </div>
       <button className='category-button'> <GoPlus className='button-icon' /> Add category</button>
     </div>
